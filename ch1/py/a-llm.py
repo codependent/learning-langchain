a2eb9b2036009e1f388e9f1b11a5f33c289e79ba
codependent <joseantonio.inigo@gmail.com>
2025-03-30 23:38:10 +0200

    updates

--- ch1/py/a-llm.py
+++ ch1/py/a-llm.py
@@ -1,6 +1,8 @@
+from langchain_ollama import ChatOllama
 from langchain_openai.chat_models import ChatOpenAI
 
-model = ChatOpenAI(model="gpt-3.5-turbo")
+#model = ChatOpenAI(model="gpt-3.5-turbo")
+model = ChatOllama(model="mistral")
 
 response = model.invoke("The sky is")
 print(response.content)
